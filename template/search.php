ad407db620bb28cd1800b26fb6039ae1787974d5
org100h <org100h@DESKTOP-8GULGUG.>
2024-02-17 08:26:47 +0000

    fix typos, formation

--- template/search.php
+++ template/search.php
@@ -1,20 +1,39 @@
-<div id="app" class="ss_search <?= $atts['post-type'] ?>" 
+<div 
+    id="app" 
+    class="ss_search <?= $atts['post-type'] ?>" 
     data-post-type="<?= $atts['post-type'] ?>"  
     data-element-count="<?= $elementCount ?>"
-    data-element-view="<?= $view ?>"
-    >
+    data-element-view="<?= $view ?>">
     <div class="ss__search-box">
-       
-        <input type="text" v-model="searchText" placeholder="Enter your search query" class="ss__input">
+        <input 
+            type="text"
+            v-model="searchText"  
+            @keyup.enter="search" 
+            placeholder="Enter your search query" 
+            class="ss__input">
         <button @click="search"><?= __('Search', 'ss'); ?></button>
-        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
-        <button class="ss__grid-toogle" @click="toggleGrid">Toggle Grid</button>
+        <div 
+            v-if="errorMessage" 
+            class="error-message"
+        >{{ errorMessage }}</div>
+        <button 
+            class="ss__grid-toogle" 
+            @click="toggleGrid">
+            <?= __('Toggle Grid', 'ss') ?></button>
     </div>
-    <div class="ss__wrapper" :class="{ 'grid': isGridEnabled || viewType === 'grid' }">
-        <div v-for="result in results" :key="result.link"  class="ss__item" >
+    <div 
+        class="ss__wrapper" 
+        :class="{ 'grid': isGridEnabled || viewType === 'grid' }">
+        <div 
+            v-for="result in results" 
+            :key="result.link"  
+            class="ss__item" >
                <h3><a :href="result.link">{{ result.title }}</a></h3>
                <p v-html="result.content"></p>
         </div>
     </div>
-        <button v-if="currentPage <= pages" @click="loadMore"><?= __('Load More', 'ss'); ?></button>
-    </div>
+        <button 
+            v-if="currentPage <= pages" 
+            @click="loadMore">
+            <?= __('Load More', 'ss'); ?></button>
+    </div>  
